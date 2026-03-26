1b7069057b95a674d8c41b3903f2324e1cd68b07
rshimode-r <rio.shimode@jp.ricoh.com>
2026-03-26 19:06:47 +0900

    英語で検索することは想定しない

--- lib/predict/autocomplete.ts
+++ lib/predict/autocomplete.ts
@@ -5,12 +5,12 @@ import { hiraToKata } from "./normalizeKana";
 export function predictPokemon(input: string) {
     if (!input) return [];
 
-    const lower = input.toLowerCase();
+    // const lower = input.toLowerCase();
     const kata = hiraToKata(input);
 
     return pokemonMap.filter((p) => {
         return (
-            p.english.startsWith(lower) ||
+            // p.english.startsWith(lower) ||
             p.japanese.startsWith(kata)
         );
     });
